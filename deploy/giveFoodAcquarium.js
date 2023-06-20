f099563619141d31595b4f1d6c95f6708d00a644
gabriele <gabriele.lodato@itsvil.it>
2023-06-20 14:03:20 +0200

    finally updated code as it has to be (i'm proud)

--- deploy/giveFoodAcquarium.js
+++ deploy/giveFoodAcquarium.js
@@ -3,42 +3,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.lambdaHandler = void 0;
 const client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
 const REGION = process.env.REGION;
-// DB client
-const ddbClient = new client_dynamodb_1.DynamoDBClient({ region: REGION, endpoint: "http://localhost:4566" });
+const ENDPOINT = "http://localhost:4566";
+const TABLE_NAME = "Acquarium";
 const ACQUARIUM_QUEUE_NAMES = ["tropical_fish_aq", "red_fish_aq", "shark_aq"];
-const lambdaHandler = async ( /*event: any*/) => {
+const client = new client_dynamodb_1.DynamoDBClient({ region: REGION, endpoint: ENDPOINT });
+const lambdaHandler = async () => {
+    const tank = "red_fish_aq";
+    const lastEat = getRandomHourMinute() + "hh:mm";
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            tank: { S: tank },
+        },
+        UpdateExpression: "SET lastEat = :lastEat",
+        ExpressionAttributeValues: {
+            ":lastEat": { S: lastEat },
+        },
+        ReturnValues: "UPDATED_NEW",
+    };
     try {
-        const scanCommand = new client_dynamodb_1.ScanCommand({
-            TableName: "Acquarium",
-        });
-        const scanResponse = await ddbClient.send(scanCommand);
-        if (scanResponse.Items && scanResponse.Items.length > 0) {
-            for (const item of scanResponse.Items) {
-                const tank = item.tank.S;
-                if (tank === "red_fish_aq") {
-                    const updatedItem = {
-                        TableName: "Acquarium",
-                        Item: {
-                            tank: { S: item.tank.S || "" },
-                            O2: { S: item.O2.S || "" },
-                            lastEat: { S: getRandomHourMinute() },
-                            waterChange: { S: item.waterChange.S || "" },
-                            Active: { BOOL: item.Active.BOOL || false },
-                            Daytime: { BOOL: item.Daytime.BOOL || false },
-                            timeStamp: { N: item.timeStamp.N || "0" },
-                        },
-                    };
-                    await ddbClient.send(new client_dynamodb_1.PutItemCommand(updatedItem));
-                    console.log(`Updated lastEat for tank: ${tank}`);
-                }
-            }
-        }
-        else {
-            console.log("No tank items found");
-        }
+        const command = new client_dynamodb_1.UpdateItemCommand(params);
+        const response = await client.send(command);
+        console.log("Update succeeded:", JSON.stringify(response));
     }
     catch (error) {
-        console.error(error);
+        console.error("Unable to update item. Error:", error);
     }
 };
 exports.lambdaHandler = lambdaHandler;
@@ -48,5 +37,10 @@ function getRandomHourMinute() {
     const hourString = hour.toString().padStart(2, "0");
     const minuteString = minute.toString().padStart(2, "0");
     return `${hourString}:${minuteString}`;
+}
+function getValue(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 (0, exports.lambdaHandler)();
--- src/functions/giveFoodAcquarium.ts
+++ src/functions/giveFoodAcquarium.ts
@@ -1,45 +1,34 @@
-import { DynamoDBClient, ScanCommand, PutItemCommand } from "@aws-sdk/client-dynamodb"
+import { DynamoDBClient, UpdateItemCommand, AttributeValue } from "@aws-sdk/client-dynamodb"
 
 const REGION = process.env.REGION
-
-// DB client
-const ddbClient = new DynamoDBClient({ region: REGION, endpoint: "http://localhost:4566" })
+const ENDPOINT = "http://localhost:4566"
+const TABLE_NAME = "Acquarium"
 const ACQUARIUM_QUEUE_NAMES = ["tropical_fish_aq", "red_fish_aq", "shark_aq"]
 
-export const lambdaHandler = async (/*event: any*/) => {
+const client = new DynamoDBClient({ region: REGION, endpoint: ENDPOINT })
+
+export const lambdaHandler = async () => {
+  const tank = "red_fish_aq"
+  const lastEat = getRandomHourMinute() + "hh:mm"
+
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      tank: { S: tank },
+    },
+    UpdateExpression: "SET lastEat = :lastEat",
+    ExpressionAttributeValues: {
+      ":lastEat": { S: lastEat },
+    },
+    ReturnValues: "UPDATED_NEW",
+  }
+
   try {
-    const scanCommand = new ScanCommand({
-      TableName: "Acquarium",
-    })
-
-    const scanResponse = await ddbClient.send(scanCommand)
-
-    if (scanResponse.Items && scanResponse.Items.length > 0) {
-      for (const item of scanResponse.Items) {
-        const tank = item.tank.S
-        if (tank === "red_fish_aq") {
-          const updatedItem = {
-            TableName: "Acquarium",
-            Item: {
-              tank: { S: item.tank.S || "" },
-              temperature: { S: getValue(10, 35).toString() + "Celsius" },
-              O2: { S: item.O2.S || "" },
-              lastEat: { S: getRandomHourMinute() },
-              waterChange: { S: item.waterChange.S || "" },
-              Active: { BOOL: item.Active.BOOL || false },
-              Daytime: { BOOL: item.Daytime.BOOL || false },
-              timeStamp: { N: item.timeStamp.N || "0" },
-            },
-          }
-          await ddbClient.send(new PutItemCommand(updatedItem))
-          console.log(`Updated lastEat for tank: ${tank}`)
-        }
-      }
-    } else {
-      console.log("No tank items found")
-    }
+    const command = new UpdateItemCommand(params)
+    const response = await client.send(command)
+    console.log("Update succeeded:", JSON.stringify(response))
   } catch (error) {
-    console.error(error)
+    console.error("Unable to update item. Error:", error)
   }
 }
 
@@ -56,7 +45,7 @@ function getRandomHourMinute(): string {
 function getValue(min: number, max: number) {
   min = Math.ceil(min)
   max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1) + min) // The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 lambdaHandler()
